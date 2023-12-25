e92ab51ec95b5a5f91543ae38e76093087e3bee3
adayaTovim <adaya012@gamail.com>
2023-12-25 10:06:29 +0200

    start study Flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import flask, render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
